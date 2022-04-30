ea73a2b3c17e02eab462b9d4775d7fe6093a8212
Kip <Kipling.Dunlap@gmail.com>
2022-04-29 23:11:23 -0400

    rotate colors, but not that

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
-document.querySelectorAll('.btn').forEach(item => {
-  item.addEventListener('click', event => { item.className = 'btn btn-block btn-warning'})
+document.querySelectorAll('.btn').forEach(item => { item.addEventListener('click', chgColor)
 });
 
+colors = ['btn btn-block btn-warning', 'btn btn-block btn-success', 'btn btn-block btn-primary']
 
+function chgColor () {
+  color = colors.shift();
+  colors.push(color);
+  document.querySelectorAll('btn').className = color.value;
+  console.log(color)
+};
+
+// use Temporal js or Moment.js to get current month, month.length
+
+// Array of NPUs, starting from 1st day of month, push to calendar grid.
